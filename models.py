a9cfe22eccb4bbc68f987d73ff1ffed5eff60589
iamdanielv <1767914+iamdanielv@users.noreply.github.com>
2025-06-22 19:39:39 -0700

    cleanup models
    cleanup use of optional, no need for none

--- models.py
+++ models.py
@@ -4,21 +4,19 @@ from enum import Enum
 from typing import List, Optional
 from pydantic import BaseModel
 
-
 class Tags(Enum):
     """Tags to be used for documentation"""
     APP_NAME = "TODOAPP"
     ROOT = "root"
     API = "API"
 
-
 class Todo(BaseModel):
     """A Todo item"""
 
-    id: Optional[int] | None = 0
+    id: Optional[int] = 0
     title: str
-    description: Optional[str] | None = ""
-    is_done: bool | None = False
+    description: Optional[str] = ""
+    is_done: Optional[bool]  = False
 
 
 class TodoMessage(BaseModel):
@@ -27,13 +25,11 @@ class TodoMessage(BaseModel):
     todo: Optional[Todo]
     message: str
 
-
 class TodoList(BaseModel):
     """Holds a Todo List"""
 
     title: str
     todos: List[Todo]
-
 
 class DefaultMessage(BaseModel):
     """A Default message"""
